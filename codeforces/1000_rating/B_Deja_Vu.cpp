613dce4ade3648da786f88dc490132233b609538
arijeetmukherjee3108-droid <arijeet.mukherjee3108@gmail.com>
2026-03-18 14:51:42 +0530

    Solved CF Problem

---
+++ codeforces/1000_rating/B_Deja_Vu.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,q;
+        cin >> n >> q;
+        vector<long long> v(n);
+        for(int i = 0; i < n; i++){
+            cin >>> v[i]; }
+        vector<int> a(q);
+        for(int i = 0; i < q; i++){
+            cin >> a[i]; }
+        
+    }
+    return 0;
+}
+
+
