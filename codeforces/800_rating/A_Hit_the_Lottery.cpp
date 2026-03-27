f3c8b2d7d2e5513ce44f2369bb38600261f66a57
arijeetmukherjee3108-droid <arijeet.mukherjee3108@gmail.com>
2026-03-27 23:30:44 +0530

    Solved CF Problem

---
+++ codeforces/800_rating/A_Hit_the_Lottery.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    while(t--){
+    int n;
+    cin >> n;
+    int ans = 0;
+    ans += n/100;
+    n %= 100;
+    ans += n/20;
+    n %= 20;
+    ans += n/10;
+    n %= 10;
+    ans += n/5;
+    n %= 5;
+    ans += n;
+    cout << ans << endl;
+}
+return 0;
+}
