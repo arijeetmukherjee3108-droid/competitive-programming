2ad0a4bb65d1d3494e3ce2a8e6470b28c55cc780
arijeetmukherjee3108-droid <arijeet.mukherjee3108@gmail.com>
2026-09-18 02:19:30 +0530

    Solved CF Problem

---
+++ codeforces/1400_rating/C_Fadi_and_LCM.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+vector<int> sieve(int n){
+    vector<bool> is_prime(n + 1, true); 
+    is_prime[0] = is_prime[1] = false;
+    
+    for(int i = 2; i * i <= n; i++){
+        if(is_prime[i]){
+            for(int j = i * i; j <= n; j += i){
+                is_prime[j] = false; 
+            }
+        }
+    }
+    
+    vector<int> primes;
+    for(int i = 2; i <= n; i++){
+        if(is_prime[i]){
+            primes.push_back(i);
+        }
+    }
+    
+    return primes;
+}
+
+vector<long long> prime_factorization(long long n){
+    vector<long long> facts;
+    for(long long i = 2; i*i <= n; i++){
+        while(n % i == 0){
+            facts.push_back(i);
+            n /= i;
+        }
+    }
+    if(n > 1){
+        facts.push_back(n);
+    }
+    
+    return facts;
+}
+
+long long power(long long a, long long b){
+    if(b == 0){
+        return 1;
+    }
+
+    long long half_power = power(a, b/2);
+    if(b % 2 == 0){
+        return (half_power * half_power) % MOD;
+    }
+    else{
+        return (((half_power * half_power) % MOD) * (a % MOD)) % MOD;
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t = 1;
+    while(t--){
+        long long x;
+        cin >> x;
+        long long ans_a = 1, ans_b = x;
+        long long start = sqrt(x);
+        for(long long i = start; i >= 1; i--){
+            if(x % i == 0){
+                long long a = i;
+                long long b = x/i;
+                if(gcd(a,b) == 1){ 
+                    ans_a = a;
+                    ans_b = b;
+                    break; }}}
+        cout << ans_a << " " << ans_b << endl;
+    }
+    return 0;
+}
